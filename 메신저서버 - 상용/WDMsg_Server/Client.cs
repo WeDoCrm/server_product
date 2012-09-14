abdf3d740a4a9d2f22fbb159f47fefb859cb2587
WeDoCrm <eclues.help@eclues.net>
2012-09-15 07:56:45 +0900

    WDMsg_Server_20120303

---
+++ 메신저서버 - 상용/WDMsg_Server/Client.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections;
+
+namespace WDMsgServer
+{
+    public class Client
+    {
+        private string id = null;
+        private string name = null;
+        private string team = null;
+        private string passwd = null;
+        private string position = null;
+        private string com_code = null;
+        private string company = null;
+        
+
+        public Client(string id, string name, string team, string extension, string com_nm, string com_cd)
+        {
+            this.id = id;
+            this.name = name;
+            this.team = team;
+            this.position = extension;
+            this.company = com_nm;
+            this.com_code = com_cd;
+           
+        }
+        
+        public string getId()
+        {
+            return id;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getTeam()
+        {
+            return team;
+        }
+
+        public string getPasswd()
+        {
+            return passwd;
+        }
+
+        public string getPosition()
+        {
+            return position;
+        }
+
+        public void setPosition(string ext)
+        {
+            position = ext;
+        }
+
+        public string getCompany()
+        {
+            return company;
+        }
+
+        public string getComCode()
+        {
+            return com_code;
+        }
+    }
+}
---
+++ 메신저서버 - 상용/WDMsg_Server/SIPMessage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WDMsgServer
+{
+    class SIPMessage
+    {
+        string Status_Code = "Unknown";
+        string Method_Name = "Unknown";
+        string Call_ID = "Unknown";
+        string Cseq = "Unknown";
+        string FromDN = "Unknown";
+        string ToDN = "Unknown";
+        string User_Agent = "Unknown";
+        string Session_Name = "Unknown";
+
+        public void setSIPMessage(string code, string method, string callid, string cseq, string from, string to, string agent, string sName)
+        {
+            Status_Code = code;
+            Method_Name = method;
+            Call_ID = callid;
+            Cseq = cseq;
+            FromDN = from;
+            ToDN = to;
+            User_Agent = agent;
+            Session_Name = sName;
+        }
+
+        public string code
+        {
+            get { return Status_Code; }
+        }
+
+        public string method
+        {
+            get { return Method_Name; }
+        }
+
+        public string callid
+        {
+            get { return Call_ID; }
+        }
+
+        public string cseq
+        {
+            get { return Cseq; }
+        }
+
+        public string from
+        {
+            get { return FromDN; }
+        }
+
+        public string to
+        {
+            get { return ToDN; }
+        }
+
+        public string agent
+        {
+            get { return User_Agent; }
+        }
+
+        public string sName
+        {
+            get { return Session_Name; }
+        }
+    }
+}
